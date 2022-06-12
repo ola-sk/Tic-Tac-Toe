f438ddcba4eec0ce29e72f77ef0c07cfdc5cd576
dawole94 <dawid.oleksy94@gmail.com>
2022-06-12 21:50:05 +0200

    merge

--- coordinates.py
+++ coordinates.py
@@ -24,18 +24,7 @@ def get_human_coordinates(board, current_player):
   should stop.
   """
   pass
-    """
-    Should return the read coordinates for the tic tac toe board from the terminal.
-    The coordinates should be in the format  letter, number where the letter is
-    A, B or C and the number 1, 2 or 3.
-    If the user enters an invalid coordinate (like Z0 or 1A, A11, sadfdsaf)
-    then a warning message should appear and the coordinates reading process repeated.
-    If the user enters a coordinate that is already taken on the board
-    then a warning message should appear and the coordinates reading process repeated.
-    If the user enters the word "quit" in any format of capitalised or non-capitalised letters, the program
-    should stop.
-    """
-    pass
+    
 
 
 def get_random_ai_coordinates(board, current_player):
