956b7f393212bfad8140b0acf811b651e71de76c
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-06-12 21:32:01 +0200

    Resolve merge conflict.

--- coordinates.py
+++ coordinates.py
@@ -1,44 +1,28 @@
 def get_human_coordinates(board, current_player):
-<<<<<<< HEAD
-
-  letter = ""
-  number = 0
-
-  while ((letter != "A" and letter != "B" and letter != "C") or (number != 1 and number != 2 and number != 3)):
-    
-    letter = input("Enter a line A, B or C: ")
-    number = int(input("Enter a column 1, 2 or 3: "))
-  
-    if ((letter != "A" and letter != "B" and letter != "C") or (number != 1 and number != 2 and number != 3)):
-      print("Warning! Invalid input!")
-    else:
-      print(f"You have chosen {letter}{number} ")
-  """
-  Should return the read coordinates for the tic tac toe board from the terminal.
-  The coordinates should be in the format  letter, number where the letter is 
-  A, B or C and the number 1, 2 or 3.
-  If the user enters an invalid coordinate (like Z0 or 1A, A11, sadfdsaf) 
-  than a warning message should appear and the coordinates reading process repeated.
-  If the user enters a coordinate that is already taken on the board.
-  than a warning message should appear and the coordinates reading process repeated.
-  If the user enters the word "quit" in any format of capitalized letters the program
-  should stop.
-  """
-  pass
-=======
     """
     Should return the read coordinates for the tic tac toe board from the terminal.
     The coordinates should be in the format  letter, number where the letter is
     A, B or C and the number 1, 2 or 3.
     If the user enters an invalid coordinate (like Z0 or 1A, A11, sadfdsaf)
-    then a warning message should appear and the coordinates reading process repeated.
-    If the user enters a coordinate that is already taken on the board
-    then a warning message should appear and the coordinates reading process repeated.
-    If the user enters the word "quit" in any format of capitalised or non-capitalised letters, the program
+    than a warning message should appear and the coordinates reading process repeated.
+    If the user enters a coordinate that is already taken on the board.
+    than a warning message should appear and the coordinates reading process repeated.
+    If the user enters the word "quit" in any format of capitalized letters the program
     should stop.
     """
-    pass
->>>>>>> 3368173c61ad8b3f6ed8ca697b6b467a0dc1d9e0
+    letter = ""
+    number = 0
+
+    while ((letter != "A" and letter != "B" and letter != "C") or (number != 1 and number != 2 and number != 3)):
+
+        letter = input("Enter a line A, B or C: ")
+        number = int(input("Enter a column 1, 2 or 3: "))
+
+    if ((letter != "A" and letter != "B" and letter != "C") or (number != 1 and number != 2 and number != 3)):
+        print("Warning! Invalid input!")
+    else:
+        print(f"You have chosen {letter}{number} ")
+
 
 
 def get_random_ai_coordinates(board, current_player):
@@ -49,7 +33,9 @@ def get_random_ai_coordinates(board, current_player):
     If the board is full (all spots taken by either X or O) then "None"
     should be returned.
     """
-    pass
+    for i in board
+
+    return (1, 2)
 
 def get_unbeatable_ai_coordinates(board, current_player):
     """
@@ -93,7 +79,7 @@ if __name__ == "__main__":
         ["X", "O", "X"],
     ]
     print("The printed coordinate should be None")
-    print(get_random_ai_coordinates(board_3))
+    print(get_random_ai_coordinates(board_3, "O"))
 
     board_4 = [
         [".", "O", "."],
